56e4373d112260ddaebc28b52941c0d58f2de6b1
geraldombuthia <geraldombuthia@gmail.com>
2023-02-25 15:25:30 +0300

    Removed useEFfect

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 const Footer = ({ quotes }) => {
   const [index, setIndex] = useState(0);
 
