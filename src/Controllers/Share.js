02d90615c35a011eaab0c7d9957556d036cb72b3
geraldombuthia <geraldombuthia@gmail.com>
2023-02-28 16:12:24 +0300

    Capitalise first letter of the word to be shared

--- src/Controllers/Share.js
+++ src/Controllers/Share.js
@@ -1,9 +1,9 @@
 const Share = (word) => {
     let shareTag = document.createElement("a")
-    let text = `*Word of the day!*%0A%0A${word}%0A%0D\nView more on: https://wordplusplus.netlify.app`
+    let text = `*Word of the day!*%0A%0A${word.charAt(0).toUpperCase() + word.slice(1)}%0A%0D\nView more on: https://wordplusplus.netlify.app`
     shareTag.setAttribute("href", `whatsapp://send?text=${text}`);
     shareTag.setAttribute("data-action", "share/whatsapp/share");
-    shareTag.click();
+    shareTag.click(); 
     console.log(encodeURIComponent(text));
 }
 // There is a Web Share API available to allow for native access
