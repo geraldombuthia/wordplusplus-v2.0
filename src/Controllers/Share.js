6ab53b463d549bda7d241b777a449d23145156fd
geraldombuthia <geraldombuthia@gmail.com>
2023-02-28 14:29:24 +0300

    Try fixing the hatsapp share functionality

--- src/Controllers/Share.js
+++ src/Controllers/Share.js
@@ -5,7 +5,7 @@ const Share = (word) => {
     shareTag.setAttribute("href", `whatsapp://send?text= *Word of the day!* &#13 ${word} &#13 View more on: https://wordplusplus.netlify.app`);
     shareTag.setAttribute("data-action", "share/whatsapp/share");
     shareTag.click();
-    console.log("clicked");
+    console.log(shareTag);
 }
 // There is a Web Share API available to allow for native access
 // to share to various different Apps available on mobile or desktop
