cacb343bd1a1368e6efb590879026317ecd9874b
geraldombuthia <geraldombuthia@gmail.com>
2023-02-28 15:21:07 +0300

    Tried making the Whatsapp to fix.

--- src/Controllers/Share.js
+++ src/Controllers/Share.js
@@ -1,6 +1,6 @@
 const Share = (word) => {
     let shareTag = document.createElement("a")
-    shareTag.setAttribute("href", `whatsapp://send?text= *Word of the day!* &#13 ${word} &#13 View more on: https://wordplusplus.netlify.app`);
+    shareTag.setAttribute("href", `whatsapp://send?text=*Word of the day!*&#13${word}&#13View more on: https://wordplusplus.netlify.app`);
     shareTag.setAttribute("data-action", "share/whatsapp/share");
     shareTag.click();
     console.log(shareTag);
