8d0e086648e71141e88ee3b086b2945799365376
geraldombuthia <geraldombuthia@gmail.com>
2023-03-03 09:22:47 +0300

    Changed the navbar contents

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-regular-svg-icons'
 const Navbar = () => {
   return (
     <div className="nav">
-        <h1>Word++</h1>
+      <h1>Search</h1>
+      <div className="nav-icons">
+        <FontAwesomeIcon icon={faBookmark} />
+      </div>
     </div>
-    
   )
 }
 
